48bd494bdc6ef732aab5340636852aea7ab9837b
alexoa.oa <alex00yt@gmail.com>
2022-07-04 11:44:03 -0500

    Adding style

--- src/public/js/agendar.js
+++ src/public/js/agendar.js
@@ -1,36 +1,66 @@
 'use strict';
 
-    const btnAgendar = document.getElementById('btnAgendar');
-    const nombreCliente = document.getElementById('nombreCliente');
-    const nombreMascota = document.getElementById('nombreMascota');
-    const tamanoMascota = document.getElementById('tamanoMascota');
-    const emailCliente = document.getElementById('emailCliente');
-    const numeroTelefono = document.getElementById('numeroTelefono');
+const btnAgendar = document.getElementById('btnAgendar');
+const nombreCliente = document.getElementById('nombreCliente');
+const nombreMascota = document.getElementById('nombreMascota');
+const razaMascota = document.getElementById('razaMascota');
+const tamanoMascota = document.getElementById('tamanoMascota');
+const numeroTelefono = document.getElementById('numeroTelefono');
+const segundoNumTel = document.getElementById('segundoNumTel');
+const inputBanoCorte = document.getElementById('inputBanoCorte');
+const inputBano = document.getElementById('inputBano');
+const fechaCita = document.getElementById('fechaCita');
+const horaCita = document.getElementById('horaCita');
+const precioServicio = document.getElementById('precioServicio');
 
 
-    //Btn de agendar cita
-    btnAgendar.addEventListener('click', (e)=>{
-        e.preventDefault();
+//Btn de agendar cita
+/* btnAgendar.addEventListener('click', (e) => {
+    e.preventDefault();
 
-        if (nombreCliente.value === '' || nombreMascota.value === '' || tamanoMascota.value === '' || emailCliente.value === '' || numeroTelefono.value === '') {
-            return console.log('Empty input, please fill all of them');
-        }
-        
-        axios.post('/nueva-cita', {
-            nombreCliente : nombreCliente.value,
-            nombreMascota : nombreMascota.value,
-            tamanoMascota : tamanoMascota.value,
-            emailCliente : emailCliente.value,
-            numeroTelefono : numeroTelefono.value,
-            segundoTelefono : 'N/A'
-        })
-        .then((response)=>{
+    if (nombreCliente.value === '' || nombreMascota.value === '' || razaMascota.value === '' || tamanoMascota.value === '' || numeroTelefono.value === '' || segundoNumTel.value === '' || (inputBanoCorte.checked === false && inputBano.checked === false) || fechaCita.value === '' || horaCita.value === '' || precioServicio.value === '') {
+        return console.log('Empty input, please fill all of them');
+    }
+
+    let servicio;
+    if (inputBano.checked === true && inputBanoCorte.checked === false) servicio = inputBano.value;
+    else if (inputBano.checked === false && inputBanoCorte.checked === true) servicio = inputBanoCorte.value;
+
+    axios.post('/nueva-cita', {
+        nombreCliente: nombreCliente.value,
+        nombreMascota: nombreMascota.value,
+        razaMascota: razaMascota.value,
+        tamanoMascota: tamanoMascota.value,
+        numeroTelefono: numeroTelefono.value,
+        segundoNumTel: segundoNumTel.value,
+        servicio: servicio,
+        fechaCita: fechaCita.value,
+        horaCita: horaCita.value,
+        precioServicio: precioServicio.value
+    })
+        .then((response) => {
             if (response.status === 200) {
-                window.location.href= '/confirmacion-cita';
+                
+                console.log(response.data.nombreMascota);
+
             }
+
         })
-        .catch((err)=>{
+        .catch((err) => {
             console.log(err);
         });
 
-    });
+}); */
+
+inputBanoCorte.addEventListener('click', () => {
+    if (inputBanoCorte.checked === true) {
+        document.getElementById('liBano').style.outline = 'none';
+        document.getElementById('liBanoCorte').style.outline = '2px solid #404040';
+    }
+});
+inputBano.addEventListener('click', () => {
+    if (inputBano.checked === true) {
+        document.getElementById('liBanoCorte').style.outline = 'none';
+        document.getElementById('liBano').style.outline = '2px solid #404040';
+    }
+});
--- src/routes/citas.js
+++ src/routes/citas.js
@@ -3,35 +3,47 @@ import { Router } from 'express';
 import { mysqlConnection } from "../database.js"
 
 const router = Router();
-let mascota = '';
 
 router.post('/nueva-cita', (req, res) => {
-    const { idCita, nombreCliente, nombreMascota, razaMascota, tamanoMascota, numeroTelefono, segundoNumeroTelefono, servicio, fecha, hora, precio } = req.body;
-
+    const { nombreCliente, nombreMascota, razaMascota, tamanoMascota, numeroTelefono, segundoNumTel, servicio, fechaCita, horaCita, precioServicio } = req.body;
     if (!(Object.keys(req.body).length === 0)) {
-        mascota = nombreMascota;
-        console.log(req.body);
+        // console.log(req.body);
 
         const query = 'CALL citaCrearNueva(0,?,?,?,?,?,?,?,?,?,?);';
-        mysqlConnection.query(query, [nombreCliente, nombreMascota, razaMascota, tamanoMascota, numeroTelefono, segundoNumeroTelefono, servicio, fecha, hora, precio], (err, results, fields) =>{
-                if (err) {
-                    //! https://restfulapi.net/http-status-codes/
-                    res.status(400).json({error : err});
-                };
-                res.status(200).json({message : 'Cita agendada correctamente.'});
-            }
+        mysqlConnection.query(query, [nombreCliente, nombreMascota, razaMascota, tamanoMascota, numeroTelefono, segundoNumTel, servicio, fechaCita, horaCita, precioServicio], (err, results, fields) => {
+            if (err) {
+                //! https://restfulapi.net/http-status-codes/
+                res.status(400).json({ status: "error", log: err });
+            };
+            res.json({
+                status : 'Cita guardada',
+                nombreMascota : nombreMascota,
+                servicio : servicio,
+                fechaCita : fechaCita,
+                horaCita : horaCita,
+                precioServicio : precioServicio
+
+            });
+        }
         );
     } else {
-        res.status(405).json({error : 'No params'});
+        res.status(405).json({ error: 'No params' });
         console.log('No params');
     }
 });
 
-router.get('/confirmacion-cita', (req, res) => {
-    res.render('confirmacionCita',
-        {
-            nombreMascota: mascota
-        });
+/* router.post('/confirmacion-cita', (req, res) => {
+    const { nombreMascota, servicio, fechaCita, horaCita, precioServicio } = req.body;
+    console.log(req.body);
+    res.status(200).render('confirmacionCita', {
+        message: 'Cita agendada!',
+        nombreMascota: nombreMascota,
+        servicio: servicio,
+        fechaCita: fechaCita,
+        horaCita: horaCita,
+        precioServicio: precioServicio
+    });
 });
+ */
 
 export default router;
